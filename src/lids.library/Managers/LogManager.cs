6c20fc2e7abaa0b4e34ac7fb41ec421cec714eba
Jarred Capellman <jcapellman@hotmail.com>
2017-04-18 19:27:36 -0500

    Added: Basic logging support

---
+++ src/lids.library/Managers/LogManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace lids.library.Managers
+{
+    public class LogManager
+    {
+        private readonly StringBuilder _sbLog;
+        private readonly string _logName;
+
+        public LogManager()
+        {
+            _sbLog = new StringBuilder();
+            _logName = $"{DateTime.Now.Ticks}.log";
+        }
+
+        public void WriteMessage(string msg)
+        {
+            _sbLog.Append($"{DateTime.Now} - {msg}");
+        }
+
+        public void FlushLog()
+        {
+            File.WriteAllText(_logName, _sbLog.ToString());
+        }
+    }
+}
--- src/lids.library/lidsmain.cs
+++ src/lids.library/lidsmain.cs
@@ -1,22 +1,35 @@
-﻿using lids.library.DAL;
+﻿using System.Net;
+
+using lids.library.DAL;
+using lids.library.Managers;
 
 namespace lids.library
 {
     public class lidsmain
     {
         private SQLiteDAL _sqlLie;
+        public static LogManager _logManager;
 
         public lidsmain(string connectionString)
         {
+            _logManager =new LogManager();
+
             _sqlLie = new SQLiteDAL(connectionString);
         }
 
         public void MainLoop()
         {
+            var hostName = Dns.GetHostName();
+
+            _logManager.WriteMessage($"Grabbing hostname...{hostName}");
+            
             while (true)
             {
                 // Check for changes and process
+                   
             }
+
+            _logManager.FlushLog();
         }
     }
 }
